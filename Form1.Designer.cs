fa8abe431fdf08a68428b3c87c25437426588d90
Evgenii Suntcov <suntsov.evgeniy@gmail.com>
2021-08-30 10:39:01 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.publicKeyATextBox = new System.Windows.Forms.TextBox();
+            this.publicKeyBTextBox = new System.Windows.Forms.TextBox();
+            this.secretKeyTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.createSecretKey = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(893, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать свой открытый 🔑";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // publicKeyATextBox
+            // 
+            this.publicKeyATextBox.Location = new System.Drawing.Point(206, 65);
+            this.publicKeyATextBox.Name = "publicKeyATextBox";
+            this.publicKeyATextBox.Size = new System.Drawing.Size(705, 27);
+            this.publicKeyATextBox.TabIndex = 1;
+            this.publicKeyATextBox.TextChanged += new System.EventHandler(this.publicKeyATextBox_TextChanged);
+            // 
+            // publicKeyBTextBox
+            // 
+            this.publicKeyBTextBox.Location = new System.Drawing.Point(206, 115);
+            this.publicKeyBTextBox.Name = "publicKeyBTextBox";
+            this.publicKeyBTextBox.Size = new System.Drawing.Size(705, 27);
+            this.publicKeyBTextBox.TabIndex = 2;
+            this.publicKeyBTextBox.TextChanged += new System.EventHandler(this.publicKeyBTextBox_TextChanged);
+            // 
+            // secretKeyTextBox
+            // 
+            this.secretKeyTextBox.Location = new System.Drawing.Point(18, 209);
+            this.secretKeyTextBox.Name = "secretKeyTextBox";
+            this.secretKeyTextBox.Size = new System.Drawing.Size(893, 27);
+            this.secretKeyTextBox.TabIndex = 3;
+            this.secretKeyTextBox.TextChanged += new System.EventHandler(this.secretKeyTextBox_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(18, 265);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(354, 201);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(557, 265);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(354, 201);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Свой открытый ключ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Открытый ключ абонента";
+            // 
+            // createSecretKey
+            // 
+            this.createSecretKey.Location = new System.Drawing.Point(18, 161);
+            this.createSecretKey.Name = "createSecretKey";
+            this.createSecretKey.Size = new System.Drawing.Size(893, 30);
+            this.createSecretKey.TabIndex = 7;
+            this.createSecretKey.Text = "Сформировать секретный ключ🗝";
+            this.createSecretKey.UseVisualStyleBackColor = true;
+            this.createSecretKey.Click += new System.EventHandler(this.createSecretKey_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(378, 265);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 86);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Зашифровать →";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(378, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(173, 82);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Расшифровать ←";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 493);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.createSecretKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.secretKeyTextBox);
+            this.Controls.Add(this.publicKeyBTextBox);
+            this.Controls.Add(this.publicKeyATextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox publicKeyATextBox;
+        private System.Windows.Forms.TextBox publicKeyBTextBox;
+        private System.Windows.Forms.TextBox secretKeyTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button createSecretKey;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {   
+        public ECDiffieHellmanCng ECDH;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ECDH = new ECDiffieHellmanCng();
+            ECDH.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            ECDH.HashAlgorithm = CngAlgorithm.Sha256;
+            publicKeyATextBox.Text = BitConverter.ToString(ECDH.PublicKey.ToByteArray());
+
+            // проверка указания открытого ключа своей стороны 
+            if (string.IsNullOrEmpty(publicKeyATextBox.Text))
+            {
+                MessageBox.Show("Свой открытый ключ не указан");
+                return;
+            }
+            // проверка указания открытого ключа второй стороны
+            if (string.IsNullOrEmpty(publicKeyATextBox.Text))
+            {
+                MessageBox.Show("Открытый ключ второй стороны не указан");
+                return;
+            }
+        }
+
+        private void publicKeyATextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void publicKeyBTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createSecretKey_Click(object sender, EventArgs e)
+        {
+            // Для обработки ошибок, возникающих при формировании 
+            // общего секретного ключа 
+            try
+            {
+                string[] strKey = publicKeyBTextBox.Text.Split('-');
+                byte[] bKey = new byte[strKey.Length];
+                for (int i = 0; i < strKey.Length; i++)
+                {
+                    uint num = uint.Parse(strKey[i], System.Globalization.NumberStyles.AllowHexSpecifier);
+                    bKey[i] = Convert.ToByte(num);
+                }
+                byte[] aKey = ECDH.DeriveKeyMaterial(CngKey.Import(bKey, CngKeyBlobFormat.EccPublicBlob));
+                // Для чтения ключа второй стороны 
+                secretKeyTextBox.Text = BitConverter.ToString(aKey);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void secretKeyTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(secretKeyTextBox.Text))
+            {
+                MessageBox.Show("Секретный ключ не сформирован");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("Исходный текст не введен");
+                return;
+            }
+            // Для обработки ошибок, возникающих при формировании 
+            // общего секретного ключа 
+            try
+            {
+                // чтение секретного ключа шифровая
+                string[] strKey = secretKeyTextBox.Text.Split('-');
+                byte[] aesKey = new byte[strKey.Length];
+                for (int i = 0; i < strKey.Length; i++)
+                {
+                    uint num =
+                    uint.Parse(strKey[i], System.Globalization.NumberStyles.AllowHexSpecifier);
+                    aesKey[i] = Convert.ToByte(num);
+                }
+                // чтение вектора инициализации
+                string[] strIV = secretKeyTextBox.Text.Split('-');
+                byte[] aesIV = new byte[16];
+                for (int i = 0; i < 16; i++)
+                {
+                    uint num =
+                    uint.Parse(strIV[i], System.Globalization.NumberStyles.AllowHexSpecifier);
+                    aesIV[i] = Convert.ToByte(num);
+                }
+                byte[] inputbuffer = Encoding.Unicode.GetBytes(richTextBox1.Text);
+                // создание экземпляр класса алгоритма AES и передача ему сформированный общего секретного ключа
+                // и вектора инициализации
+                Aes aes = Aes.Create();
+                aes.Key = aesKey;
+                aes.IV = aesIV;
+                // режим шифрования: сцепление блоков шифротекста Cipher Block Chaining (CBC)
+                aes.Mode = CipherMode.CBC;
+                ICryptoTransform transform = aes.CreateEncryptor(aesKey, aesIV);
+                byte[] outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
+                richTextBox2.Text = Convert.ToBase64String(outputBuffer);
+                // Для чтения ключа второй стороны 
+                secretKeyTextBox.Text = BitConverter.ToString(aesKey);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(secretKeyTextBox.Text))
+            {
+                MessageBox.Show("Секретный ключ не сформирован");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(richTextBox2.Text))
+            {
+                MessageBox.Show("Исходный текст не введен");
+                return;
+            }
+            // Для обработки ошибок, возникающих при формировании 
+            // общего секретного ключа 
+            try
+            {
+                // чтение секретного ключа шифровая
+                string[] strKey = secretKeyTextBox.Text.Split('-');
+                byte[] aesKey = new byte[strKey.Length];
+                for (int i = 0; i < strKey.Length; i++)
+                {
+                    uint num =
+                    uint.Parse(strKey[i], System.Globalization.NumberStyles.AllowHexSpecifier);
+                    aesKey[i] = Convert.ToByte(num);
+                }
+                // чтение вектора инициализации
+                string[] strIV = secretKeyTextBox.Text.Split('-');
+                byte[] aesIV = new byte[16];
+                for (int i = 0; i < 16; i++)
+                {
+                    uint num =
+                    uint.Parse(strIV[i], System.Globalization.NumberStyles.AllowHexSpecifier);
+                    aesIV[i] = Convert.ToByte(num);
+                }
+                // чтения закодированного текста
+                //byte[] inputbuffer = Encoding.Unicode.GetBytes(richTextBox2.Text);
+                // чтения закодированного текста
+                byte[] inputbuffer = Convert.FromBase64String(richTextBox2.Text);
+
+                // создание экземпляр класса алгоритма AES и передача ему сформированный общего секретного ключа
+                // и вектора инициализации
+                Aes aes = Aes.Create();
+                aes.Key = aesKey;
+                aes.IV = aesIV;
+                // режим шифрования: сцепление блоков шифротекста Cipher Block Chaining (CBC)
+                aes.Mode = CipherMode.CBC;
+                // Создание дешифратора
+                ICryptoTransform transform = aes.CreateDecryptor(aesKey, aesIV);
+                // расшифровка закодированного текста
+                byte[] outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
+                // вывод расшифрованного текста в текстовое поле richTextBox1
+                richTextBox1.Text = Encoding.Unicode.GetString(outputBuffer);
+                // Для чтения ключа второй стороны 
+                secretKeyTextBox.Text = BitConverter.ToString(aesKey);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
